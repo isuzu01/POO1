b86d69532d1f824c25b6eed9a4593e76d10e6a36
Jhuli Trujillo <jhulitrujillomeza@gmail.com>
2024-10-08 18:16:27 -0500

    tarea 06

---
+++ POO1_Tarea06_TrujilloMezaJhuli/POO1_Tarea06_TrujilloMezaJhuli/Controllers/ConsultaNegociosController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using POO1_Tarea06_TrujilloMezaJhuli.Models;
+
+namespace POO1_Tarea06_TrujilloMezaJhuli.Controllers
+{
+    public class ConsultaNegociosController : Controller
+    {
+        // GET: ConsultaNegocios
+
+        Negocios2022Entities db = new Negocios2022Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductoListar()
+        {
+            var lista = from p in db.usp_ProductoListar() select p;
+            return View(lista.ToList());
+        }
+
+        public ActionResult ProductoNombreListar(string nombre )
+        {
+            if(nombre == null) nombre = string.Empty;
+            ViewBag.nombre = nombre;
+            var lista = from p in db.usp_ProductoListar() where p.NombreProducto.StartsWith(nombre) select p;
+            return View(lista.ToList());
+        }
+
+        public ActionResult PedidosCliente( string id)
+        {
+            if(id == null) id = string.Empty;
+            ViewBag.cliente = new SelectList(db.tb_clientes.ToList(), "IdCliente", "NombreCia", id);
+            var lista = from p in db.tb_pedidoscabe where p.IdCliente.Equals(id) select p;
+            return View(lista.ToList());
+        }
+    }
+}
---
+++ POO1_Tarea06_TrujilloMezaJhuli/POO1_Tarea06_TrujilloMezaJhuli/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POO1_Tarea06_TrujilloMezaJhuli.MvcApplication" Language="C#" %>
